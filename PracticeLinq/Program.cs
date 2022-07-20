4672cdaf032c8025524ed4ba31e917f7f5d11fa3
coreyareid <coreyareid@bellsouth.net>
2022-07-19 19:25:06 -0500

    Completed exercise 2

--- PracticeLinq/Program.cs
+++ PracticeLinq/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
+// Exercise 2
+// List of video game names
+List<string> games = new List<string>() { "Halo", "Call of Duty", "Forza", "Minecraft", "Fight Night" };
+
+// Shows list un-ordered
+for (int i = 0; i < games.Count; i++)
+{
+    Console.WriteLine(games[i]);
+}
+
+// shows list ordered by name Length using lambda expressions
+var ordered = games.OrderBy(game => game.Length);
+
+foreach (var game in ordered)
+{
+    Console.WriteLine(game);
+}
